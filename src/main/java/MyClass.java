fbed8a8b03f7c0345b32bc589f595129f5bb8172
rcruzm <rcm_27_12@hotmail.com>
2017-01-10 10:38:48 -0500

    Modification 001

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -3,16 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.test01;
 
 /**
  *
  * @author Rene
  */
-public class Main {
-    public static void main(String args[]){
-    
-    
+public class MyClass {
+      public int multiply(int x, int y) {
+    // the following is just an example
+    if (x > 999) {
+      throw new IllegalArgumentException("X should be less than 1000");
     }
-            
+    return x / y;
+  }
 }
---
+++ target/test-classes/FirstTest.java
@@ -0,0 +1,38 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Rene
+ */
+public class FirstTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testExceptionIsThrown() {
+        MyClass tester = new MyClass();
+        tester.multiply(1000, 5);
+    }
+
+    @Test
+    public void testMultiply() {
+        MyClass tester = new MyClass();
+        assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
